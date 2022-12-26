c92387241b9775d8cc02a3bf5e8e542b0c049a13
Korolenko Sergey <skorolenko@clevertec.ru>
2022-12-26 14:17:19 +0300

    Как получать сообщения из очереди

---
+++ src/main/java/ru/clevertec/controller/SampleController.java
@@ -0,0 +1,27 @@
+package ru.clevertec.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class SampleController {
+
+    private final AmqpTemplate template;
+
+    @PostMapping("/emit")
+    public ResponseEntity<String> emit(@RequestBody String message) {
+        log.info("Emit to myQueue");
+        template.convertAndSend("myQueue", message);
+        return ResponseEntity.ok("Success emit to queue");
+    }
+}
